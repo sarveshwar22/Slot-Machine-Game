229b370753e5b8e6504ce1ed2e003494bbf74fb5
sarveshwar22 <f20190257@goa.bits-pilani.ac.in>
2020-07-20 20:21:19 +0530

    slot machine
    Signed-off-by: sarveshwar22 <f20190257@goa.bits-pilani.ac.in>

--- slot-machine-game/script.js
+++ slot-machine-game/script.js
@@ -15,3 +15,23 @@ setInterval(() => {
     value2.innerText = getRandomValue();
     value3.innerText = getRandomValue();
 }, 300);
+let animationId;
+function updateAnimation(newspeed){
+    if(animationId)
+    clearInterval(animationId)
+
+    animationId = setInterval(() => {
+        value1.innerText = getRandomValue()
+        value2.innerText = getRandomValue()
+        value3.innerText = getRandomValue()
+        
+    },1000/newspeed)
+}
+
+
+inpspeed.onchange = function(ev){
+    console.log('value changed',ev.target.value)
+    //root-document.documentelement
+    document.documentElement.style.setProperty('--speed',ev.target.value)
+    updateAnimation(ev.target.value)
+}
